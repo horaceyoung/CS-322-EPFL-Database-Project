eeb7852f44e703f81f2f8e4810d0d1dc6acd0aa6
splashingsplashes <splashingsplashes@gmail.com>
2019-05-08 11:30:34 +0200

    interface update

--- Interface WPF App/CS322 DB interface/CS322 DB interface/MainWindow.xaml.cs
+++ Interface WPF App/CS322 DB interface/CS322 DB interface/MainWindow.xaml.cs
@@ -22,12 +22,13 @@ namespace Proj
     public partial class MainWindow : Window
     {
         public string[] table_names { get; set; }
+        public string[] VerificationName { get; set; }
         public MainWindow()
         {
             InitializeComponent();
 
             table_names = new string[] { "Listing", "Host", "City", "Country", "Room Type", "Property Type" };
-
+            VerificationName = new string[] { "telephone", "email"};
             DataContext = this;
         }
 
@@ -36,6 +37,42 @@ namespace Proj
             switch(InsertTableOption.SelectedItem.ToString()){
                 case "Listing":
                     InsertListing.Visibility = Visibility.Visible;
+                    InsertHost.Visibility = Visibility.Collapsed;
+                    break;
+                case "Host":
+                    InsertHost.Visibility = Visibility.Visible;
+                    InsertListing.Visibility = Visibility.Collapsed;
+                    //HostVerificationInsert.Visibility = Visibility.Visible;
+                    //                            < Grid x: Name = "HostVerificationInsert" HorizontalAlignment = "Left" Margin = "80,10,0,0" Visibility = "Collapsed" >
+
+                    //                    < TextBlock Text = "Host Verification" />
+
+                    //                     < StackPanel >
+
+
+                    //                         < ComboBox Name = "cb" ItemsSource = "{Binding .}" Margin = "0,20,0,0" >
+
+                    //                                  < ComboBox.ItemTemplate >
+
+                    //                                      < DataTemplate >
+
+                    //                                          < StackPanel Orientation = "Horizontal" >
+
+                    //                                               < CheckBox Name = "HostVerification"  IsChecked = "{Binding IsChecked}" Checked = "CheckBox_Checked" Unchecked = "CheckBox_Unchecked" ></ CheckBox >
+
+                    //                                                      < TextBlock Text = "{Binding VerificationName}" ></ TextBlock >
+
+                    //                                                   </ StackPanel >
+
+                    //                                               </ DataTemplate >
+
+                    //                                           </ ComboBox.ItemTemplate >
+
+                    //                                       </ ComboBox >
+
+                    //                                   </ StackPanel >
+
+                    //                               </ Grid >
                     break;
                     
 
